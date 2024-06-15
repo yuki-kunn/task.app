e15901524139974cf1291c2bf8febf44c0d79655
yuki-kunn <21e1248@andrew.ac.jp>
2024-06-15 22:03:06 +0900

    一旦全部消して作り直し

---
+++ front/src/constants/linkdata.ts
@@ -0,0 +1,7 @@
+/**
+ * 静的ページなどのリンク先
+ * @type {Object}
+ */
+export const LINK_DATA = {
+    HOME_LINK: '/',
+  }
---
+++ front/src/constants/requestdata.ts
@@ -0,0 +1,10 @@
+/**
+ * HTTPリクエストのURL
+ * @type {Object}
+ */
+export const REQUEST_DATA = {
+    TODO_GET: 'http://localhost:3000/todo',
+    TODO_POST: 'http://localhost:3000/todo',
+    TODO_PUT: 'http://localhost:3000/todo',
+    TODO_DELETE: 'http://localhost:3000/todo/',
+  }
